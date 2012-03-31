cb2eed67c57da8fdb61d0abcedfefe9a6bd1581c
edikay <edikay@gmail.com>
2012-03-31 12:39:57 +0200

    poprawilem biblioteki

---
+++ src/biz/lazysoft/cdh/BaseGame.java
@@ -0,0 +1,53 @@
+package biz.lazysoft.cdh;
+
+import org.anddev.andengine.opengl.font.Font;
+import org.anddev.andengine.opengl.font.FontManager;
+import org.anddev.andengine.opengl.texture.TextureOptions;
+import org.anddev.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
+import org.anddev.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
+import org.anddev.andengine.opengl.texture.region.TextureRegion;
+import org.anddev.andengine.opengl.texture.region.TiledTextureRegion;
+import org.anddev.andengine.ui.activity.BaseGameActivity;
+
+import android.graphics.Color;
+import android.graphics.Typeface;
+
+public abstract class BaseGame extends BaseGameActivity {
+
+	public TextureRegion loadTextureRegion(String tPath, int tWidth, int tHeight) {
+		BitmapTextureAtlas tmpAtlas;
+		tmpAtlas = new BitmapTextureAtlas(tWidth, tHeight,
+				TextureOptions.BILINEAR_PREMULTIPLYALPHA);
+		TextureRegion tmpTexture;
+		tmpTexture = BitmapTextureAtlasTextureRegionFactory.createFromAsset(
+				tmpAtlas, this, tPath, 0, 0);
+		this.mEngine.getTextureManager().loadTexture(tmpAtlas);
+		return tmpTexture;
+	}
+
+	public TiledTextureRegion loadTiledTextureRegion(String tPath, int tWidth,
+			int tHeight, int tTileColumns, int tTileRows) {
+		BitmapTextureAtlas tmpAtlas;
+		tmpAtlas = new BitmapTextureAtlas(tWidth, tHeight,
+				TextureOptions.BILINEAR_PREMULTIPLYALPHA);
+		TiledTextureRegion tmpTexture;
+		tmpTexture = BitmapTextureAtlasTextureRegionFactory
+				.createTiledFromAsset(tmpAtlas, this, tPath, 0, 0,
+						tTileColumns, tTileRows);
+		this.mEngine.getTextureManager().loadTexture(tmpAtlas);
+		return tmpTexture;
+	}
+
+	public Font loadFont(int color) {
+		BitmapTextureAtlas tmpAtlas = new BitmapTextureAtlas(256, 256, TextureOptions.BILINEAR_PREMULTIPLYALPHA);
+		Font font = new Font(tmpAtlas, Typeface.create(Typeface.DEFAULT, Typeface.BOLD), 35, true, Color.WHITE);
+		this.mEngine.getTextureManager().loadTexture(tmpAtlas);
+        this.getFontManager().loadFont(font);		
+		return font;
+	}
+
+	public FontManager getFontManager() {
+		return this.mEngine.getFontManager();
+	}
+
+}
--- src/biz/lazysoft/cdh/CdhActivity.java
+++ src/biz/lazysoft/cdh/CdhActivity.java
@@ -1,9 +1,11 @@
 package biz.lazysoft.cdh;
 
-import android.app.Activity;
+import org.anddev.andengine.engine.Engine;
+import org.anddev.andengine.entity.scene.Scene;
+
 import android.os.Bundle;
 
-public class CdhActivity extends Activity {
+public class CdhActivity extends  BaseGame{
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -11,4 +13,28 @@ public class CdhActivity extends Activity {
         setContentView(R.layout.main);
         //Widzisz to?
     }
+
+	@Override
+	public Engine onLoadEngine() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void onLoadResources() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Scene onLoadScene() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void onLoadComplete() {
+		// TODO Auto-generated method stub
+		
+	}
 }

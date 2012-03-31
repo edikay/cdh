f025c22dbb7973c64c92b22430807f70497fd127
edikay <edikay@gmail.com>
2012-03-31 12:16:34 +0200

    Project commit

---
+++ src/biz/lazysoft/cdh/CdhActivity.java
@@ -0,0 +1,13 @@
+package biz.lazysoft.cdh;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CdhActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
